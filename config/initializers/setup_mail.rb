a5b1f2dbafd067de2ea6e61deaa57ac6781eb2c3
Hara Kumar Acharya <hara.acharya@gmail.com>
2014-05-18 22:09:01 -0700

    Update setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "edildo.me",
-  :user_name            => "samantha.edildo",
-  :password             => "Web@1234",
+  :user_name            => "xxxxxxxxxxxxxxx",
+  :password             => "xxxxxxxx",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
